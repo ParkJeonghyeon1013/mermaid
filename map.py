df03f57b96b430848f2028d10a4314d45f6e50f0
ParkJeonghyeon1013 <62707146+ParkJeonghyeon1013@users.noreply.github.com>
2020-08-08 21:28:58 +0900

    Map
    - return 안되던거 수정해서 성공시킴
    - stage 별 미니 게임 만든 후 main에 import 시키기

--- map.py
+++ map.py
@@ -1,23 +1,28 @@
 #Map에서 어디로 갈지 물어본
-def choose():
-    map_num = 0
+def choose(stage):
+    num=1
     stage_1 = "1 - 미역 공장 가본다"
     stage_2 = "2 - 진주 가게에 가본다"
     stage_3 = "3 - 약쟁이 소라게에게 가본다"
+#첫번째 선택
+    if stage == 0:
+        num = str(input("어디를 가볼까요?\n%s\n%s\n%s\n"%(stage_1,stage_2,stage_3)))
+        print(num)
+#두번째 선택 (진주/소라게)
+    elif stage == 1:
+        num = str(input("어디를 가볼까요?\n%s\n%s\n"%(stage_2,stage_3)))
+        print(num)
+#두번째 선택 (미역/소라게)
+    elif stage == 2:
+        num = str(input("어디를 가볼까요?\n%s\n%s\n"%(stage_1,stage_3)))
+        print(num)
+#세번째 선택 (미역/진주)
+    elif stage == 3:
+        num = str(input("어디를 가볼까요?\n%s\n%s\n"%(stage_1,stage_2)))
+        print(num)
     
-    while True:
-        map_num = int(input("어디를 가볼까요?\n%s\n%s\n%s\n"%(stage_1,stage_2,stage_3)))
-        if map_num == 1:
-            break
-        elif map_num == 2:
-            break
-        elif map_num == 3:
-            break
-        else:
-            print("다시 입력해주세요")
-            continue
-    print(map_num)
-    return(map_num)
+    return num
+
 
 def seaweed(a,b):
     print(a,b + "미역")
@@ -30,6 +35,5 @@ def pearl(a,b):
 def full_map(self):
     print("어딜 가볼까요?")
     print("1 - 미역 공장 가본다\n2 - 진주 가게에 가본다\n3 - 약쟁이 소라게에게 가본다")
-        
-choose()
+
 
